3019fff24f7ec8637229f5268a8c3fe69f8c4349
RexTheWeeb <D.F.vantwoudt@outlook.com>
2025-03-25 13:27:58 +0100

    actions file

--- webservice/includes/actions.php
+++ webservice/includes/actions.php
@@ -9,8 +9,66 @@ function getProducten(): array
     return [
 
         [
-
+            "id" => 1,
+            "naam" => "Jumbo - Boeren Tijgerbrood Bruin",
+            "soort" => "Brood",
+            "prijs" => 1.28
+        ],
+        [
+            "id" => 2,
+            "naam" => "Jumbo - Witte Bolletjes - 4 Stuks",
+            "soort" => "Brood",
+            "prijs" => 1.99
+        ],
+        [
+            "id" => 3,
+            "naam" => "Lezziz Kipborrelhap Gekruid 500 g",
+            "soort" => "Vleeswaren",
+            "prijs" => 4.00
+        ],
+        [
+            "id" => 4,
+            "naam" => "Jumbo Slavink Varken 5 Stuks",
+            "soort" => "Vleeswaren",
+            "prijs" => 4.50
+        ],
+        [
+            "id" => 5,
+            "naam" => "Jumbo Druiven Wit Pitloos 500 g",
+            "soort" => "Fruit",
+            "prijs" => 2.79
+        ],
+        [
+            "id" => 6,
+            "naam" => "Jumbo Classic Burger Rundvlees 8 Stuks",
+            "soort" => "Vleeswaren",
+            "prijs" => 9.00
+        ],
+        [
+            "id" => 7,
+            "naam" => "Jumbo Conference Peren 1KG",
+            "soort" => "Fruit",
+            "prijs" => 2.19
+        ],
+        [
+            "id" => 8,
+            "naam" => "Jumbo Garnalen Naturel 150 g",
+            "soort" => "Vleeswaren",
+            "prijs" => 4.09
+        ],
+        [
+            "id" => 9,
+            "naam" => "Jumbo - Rozijnen Krentenbollen - 6 Stuks",
+            "soort" => "Brood",
+            "prijs" => 1.69
+        ],
+        [
+            "id" => 10,
+            "naam" => "Jumbo Komkommer",
+            "soort" => "Groenten",
+            "prijs" => 0.95
         ]
+
 
     ];
 }

127ab6f412faf4c214975a71ef3af094946f2362
cctenshii <SissiWang2003@gmail.com>
2025-03-25 14:40:51 +0100

    Ik heb een begin gemaakt aan de main.js en css.

--- webservice/includes/actions.php
+++ webservice/includes/actions.php
@@ -4,69 +4,69 @@
  * @return array
  */
 
-function getProducten(): array
+function getProducts(): array
 {
     return [
 
         [
             "id" => 1,
-            "naam" => "Jumbo - Boeren Tijgerbrood Bruin",
-            "soort" => "Brood",
-            "prijs" => 1.28
+            "name" => "Jumbo - Boeren Tijgerbrood Bruin",
+            "category" => "Brood",
+            "price" => 1.28
         ],
         [
             "id" => 2,
-            "naam" => "Jumbo - Witte Bolletjes - 4 Stuks",
-            "soort" => "Brood",
-            "prijs" => 1.99
+            "name" => "Jumbo - Witte Bolletjes - 4 Stuks",
+            "category" => "Brood",
+            "price" => 1.99
         ],
         [
             "id" => 3,
-            "naam" => "Lezziz Kipborrelhap Gekruid 500 g",
-            "soort" => "Vleeswaren",
-            "prijs" => 4.00
+            "name" => "Lezziz Kipborrelhap Gekruid 500 g",
+            "category" => "Vleeswaren",
+            "price" => 4.00
         ],
         [
             "id" => 4,
-            "naam" => "Jumbo Slavink Varken 5 Stuks",
-            "soort" => "Vleeswaren",
-            "prijs" => 4.50
+            "name" => "Jumbo Slavink Varken 5 Stuks",
+            "category" => "Vleeswaren",
+            "price" => 4.50
         ],
         [
             "id" => 5,
-            "naam" => "Jumbo Druiven Wit Pitloos 500 g",
-            "soort" => "Fruit",
-            "prijs" => 2.79
+            "name" => "Jumbo Druiven Wit Pitloos 500 g",
+            "category" => "Fruit",
+            "price" => 2.79
         ],
         [
             "id" => 6,
-            "naam" => "Jumbo Classic Burger Rundvlees 8 Stuks",
-            "soort" => "Vleeswaren",
-            "prijs" => 9.00
+            "name" => "Jumbo Classic Burger Rundvlees 8 Stuks",
+            "category" => "Vleeswaren",
+            "price" => 9.00
         ],
         [
             "id" => 7,
-            "naam" => "Jumbo Conference Peren 1KG",
-            "soort" => "Fruit",
-            "prijs" => 2.19
+            "name" => "Jumbo Conference Peren 1KG",
+            "category" => "Fruit",
+            "price" => 2.19
         ],
         [
             "id" => 8,
-            "naam" => "Jumbo Garnalen Naturel 150 g",
-            "soort" => "Vleeswaren",
-            "prijs" => 4.09
+            "name" => "Jumbo Garnalen Naturel 150 g",
+            "category" => "Vleeswaren",
+            "price" => 4.09
         ],
         [
             "id" => 9,
-            "naam" => "Jumbo - Rozijnen Krentenbollen - 6 Stuks",
-            "soort" => "Brood",
-            "prijs" => 1.69
+            "name" => "Jumbo - Rozijnen Krentenbollen - 6 Stuks",
+            "category" => "Brood",
+            "price" => 1.69
         ],
         [
             "id" => 10,
-            "naam" => "Jumbo Komkommer",
-            "soort" => "Groenten",
-            "prijs" => 0.95
+            "name" => "Jumbo Komkommer",
+            "category" => "Groenten",
+            "price" => 0.95
         ]
 
 
--- webservice/index.php
+++ webservice/index.php
@@ -3,7 +3,7 @@
 require_once "includes/actions.php";
 
 if (!isset($_GET['id'])) {
-    $data = getProducten();
+    $data = getProducts();
 } else {
     $data = getProductDetails($_GET['id']);
 }
@@ -11,4 +11,3 @@ header("Content-Type: application/json");
 echo $data !== false ? json_encode($data) : json_encode(['error' => 'Not found']);
 exit;
 
-?>

18fc6f15d4b6c5583956e4bb171d71fb7cb2a4b7
AngryPapayah <airissavermo@hotmail.com>
2025-03-26 10:03:51 +0100

    Mappen structuur etc.

---
+++ werkmap-sissi/js/main.js
@@ -0,0 +1,64 @@
+// Start JS codes - Sissi
+window.addEventListener('load', init);
+
+const apiUrl = 'webservice/index.php';
+let productGallery;
+
+function init() {
+    productGallery = document.querySelector('#products-gallery');
+
+    getProductData();
+}
+
+function getProductData() {
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(createProductCards)
+
+        .catch(ajaxErrorHandler);
+}
+
+function createProductCards(data) {
+    console.log(data);
+
+    for (let product of data) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+        productCard.dataset.name = product.name;
+
+        productGallery.appendChild(productCard);
+        fillProductCard(product);
+    }
+}
+
+function fillProductCard(product) {
+    const productCard = document.querySelector(`.product-card[data-name='${product.name}']`);
+
+    const productName = document.createElement('h2');
+    productName.classList.add('name');
+    productName.innerText = `${product.name}`;
+    productCard.appendChild(productName);
+
+    const productImg = document.createElement('img');
+    productImg.src = `webservice/img/${product.id}.avif`;
+    productImg.alt = product.name;
+    productCard.appendChild(productImg);
+
+    const productPrice = document.createElement('h3');
+    productPrice.classList.add('price');
+    productPrice.innerText = `€${product.price}`;
+    productCard.appendChild(productPrice);
+}
+
+function ajaxErrorHandler(error) {
+    console.log(error);
+    const message = document.createElement('div');
+    message.classList.add('error');
+    message.innerHTML = 'Something went wrong, please try again later.';
+    productGallery.before(message);
+}

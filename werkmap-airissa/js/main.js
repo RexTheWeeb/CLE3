41e3a98a2757d9cee241db2a3da7dba42831582e
AngryPapayah <airissavermo@hotmail.com>
2025-03-26 11:36:36 +0100

    werkmap airissa beginnen... Api moet nog....

---
+++ werkmap-airissa/js/main.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', init);
+
+function init() {
+   
+}
+
+function errorHandler(error) {
+    console.log(error);
+    const message = document.createElement('div');
+    message.classList.add('error');
+    message.innerHTML = 'Er is iets fout gegaan';
+
+}
